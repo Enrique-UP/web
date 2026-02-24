3e0094e3720a0c94d25236bbf1fe241e050cc89d
Nomaan-i2a <noman.na@akounto.com>
2026-02-24 11:32:49 +0530

    new

--- hardCode/src/index.js
+++ hardCode/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { HashRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  </HashRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 reportWebVitals();
